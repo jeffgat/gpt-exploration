76a4219d54405bbc242d5bd390921f20bf9eb99b
jeffgat <jeff.gtbn@gmail.com>
2023-01-22 17:24:37 -0600

    added tailwind

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,10 @@
+self.__RSC_CSS_MANIFEST={
+  "__entry_css_mods__": {
+    "/Users/jeffreygatbonton/Desktop/Code/0-Practice/open-ai/client/app/page": [
+      "/Users/jeffreygatbonton/Desktop/Code/0-Practice/open-ai/client/app/globals.css"
+    ]
+  },
+  "/Users/jeffreygatbonton/Desktop/Code/0-Practice/open-ai/client/app/layout.tsx": [
+    "/Users/jeffreygatbonton/Desktop/Code/0-Practice/open-ai/client/app/globals.css"
+  ]
+}
